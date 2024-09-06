cb7189e783f8c2cf832ee1882125e9c17379d2ec
Harentsoa <hei.harentsoa@gmail.com>
2024-09-06 19:27:11 +0300

    fix: server

--- src/components/ListPossession.jsx
+++ src/components/ListPossession.jsx
@@ -7,9 +7,9 @@ import Button from 'react-bootstrap/Button';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './navbar';
 import './css/style.css';
-import BienMateriel from '../../models/possessions/BienMateriel';
-import Flux from '../../../models/possessions/Flux';
-import Personne from '../../../models/Personne';
+import BienMateriel from '../assets/models/possessions/BienMateriel.js';
+import Flux from '.../assets/models/possessions/Flux.js';
+import Personne from '../assets/models/possessions/Personne.js';
 const apiUrl = import.meta.env.VITE_API_URL;
 
 
